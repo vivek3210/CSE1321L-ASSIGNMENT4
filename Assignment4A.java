87d872924e43e4a2246d089c5ef2fa37dbc2b565
vivek3210 <36582094+vivek3210@users.noreply.github.com>
2022-09-25 17:24:51 -0400

    Add files via upload

--- Assignment4A.java
+++ Assignment4A.java
@@ -10,32 +10,36 @@ public class Assignment4A {
         System.out.println("[CCSE Study Abroad Survey]");
         Scanner input = new Scanner(System.in);
         int counter = 1;
-        do
+
+        while (!answer.equalsIgnoreCase("Quit"))
         {
             System.out.println("Student #" + counter + ": Which country should we go to for our next study abroad program?");
-            System.out.println("Italy\nCosta Rica\nPax Bisonica\nGhana\n(Type Quit to end the survey)");
-            answer = input.next();
-            System.out.println("Thank you!\n");
+            System.out.println("Italy\nCosta Rica\nPax Bisonica\nGhana\n(Type Quit to end the survey)\n");
+            answer = input.nextLine();
 
             if (answer.equalsIgnoreCase("Italy"))
             {
                 italyNum++;
+                System.out.println("Thank you!\n");
             }
-            else if (answer.equalsIgnoreCase("Costa Rica"))
+            if (answer.equalsIgnoreCase("Costa Rica"))
             {
                 costaNum++;
+                System.out.println("Thank you!\n");
             }
-            else if (answer.equalsIgnoreCase("Pax Bisonica"))
+            if (answer.equalsIgnoreCase("Pax Bisonica"))
             {
                 paxNum++;
+                System.out.println("Thank you!\n");
             }
-            else if (answer.equalsIgnoreCase("Ghana"))
+            if (answer.equalsIgnoreCase("Ghana"))
             {
                 ghanaNum++;
+                System.out.println("Thank you!\n");
             }
 
             counter++;
-        } while (!answer.equalsIgnoreCase("Quit"));
+        }
 
         System.out.println("[Results]");
         System.out.println("Italy: " + italyNum);
@@ -43,8 +47,24 @@ public class Assignment4A {
         System.out.println("Pax Bisonica: " + paxNum);
         System.out.println("Ghana: " + ghanaNum);
 
+        int max = Math.max(italyNum, Math.max(costaNum, Math.max(paxNum, ghanaNum)));
 
-
+        if (max == italyNum)
+        {
+            System.out.println("Next year’s study abroad program will take place in Italy.");
+        }
+        if (max == costaNum)
+        {
+            System.out.println("Next year’s study abroad program will take place in Costa Rica.");
+        }
+        if (max == paxNum)
+        {
+            System.out.println("Next year’s study abroad program will take place in Pax Bisonica.");
+        }
+        if (max == ghanaNum)
+        {
+            System.out.println("Next year’s study abroad program will take place in Ghana.");
+        }
 
 
 
